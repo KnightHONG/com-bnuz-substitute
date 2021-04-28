df3a00a58ac8cae5c1fcf1f4cdd330a26503376d
黄健洪 <3324045781@qq.com>
2021-04-28 11:26:40 +0800

    first commit

---
+++ src/main/java/com/bnuz/substitute/SubstituteApplication.java
@@ -0,0 +1,13 @@
+package com.bnuz.substitute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubstituteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubstituteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bnuz/substitute/SubstituteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnuz.substitute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubstituteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
